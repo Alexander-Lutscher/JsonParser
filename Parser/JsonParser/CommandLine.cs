47e12615403ce7ecf9577875fc9039c2a8e86c48
Raiden <Gordon.Shumway@googlemail.com>
2019-11-21 01:11:09 +0100

    JP-1 Add First CommandLineParser implementation

---
+++ Parser/JsonParser/CommandLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Pidgin;
+using System.Linq;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+namespace JsonParser
+{
+    public static class CommandLine
+    {
+        public static readonly Parser<char, KeyValuePair<string, string>> NullParser = End.ThenReturn(new KeyValuePair<string, string>("",""));
+        public static readonly Parser<char, char> SeparationCharParser = Char('=');
+        public static readonly Parser<char, char> StartingCharParser = Try(Char('/').Or(Try(Char('-'))));
+        
+        public static readonly Parser<char, KeyValuePair<string,string>> KeyOnlyCommandParser =
+        (
+            Map(
+                x => new KeyValuePair<string, string>( x , "") , 
+                Token(t => t != '=' ).ManyString().Before(End))
+        );
+        
+        public static readonly Parser<char, KeyValuePair<string,string>> KeyAndValueCommandParser =
+        (
+            Map
+            (
+                (x) =>
+                {
+                    return new KeyValuePair<string, string>(x.ElementAt(0), x.ElementAt(1));
+                },
+                LetterOrDigit.AtLeastOnceString().Separated(SeparationCharParser)
+            )
+        );
+        
+        public static readonly Parser<char, KeyValuePair<string,string>> CommandLineParser = 
+            NullParser.Or(
+                StartingCharParser.Then(Try(KeyOnlyCommandParser).Or(KeyAndValueCommandParser)));
+    }
+}
--- Parser/JsonParser/Program.cs
+++ Parser/JsonParser/Program.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Pidgin;
 
 namespace JsonParser
@@ -14,9 +15,55 @@ namespace JsonParser
     public class Test
     {
         [Test]
-        public void Test123()
+        public void TestNull()
         {
-            Assert.DoesNotThrow(() => CommandLineParser.commandLineParser.ParseOrThrow((string)null));
+            Assert.DoesNotThrow(() => CommandLine.CommandLineParser.ParseOrThrow((string)null));
+        }
+        
+        [Test]
+        public void TestNoCommand()
+        {
+            Assert.DoesNotThrow(() => CommandLine.CommandLineParser.ParseOrThrow(""));
+        }
+        
+        [Test]
+        public void TestBeginsWithSpecialCharacter()
+        {
+            Assert.DoesNotThrow(() => CommandLine.CommandLineParser.ParseOrThrow("/"));
+        }
+        
+        [Test]
+        public void TestCanParseAnyInput()
+        {
+            var aString = CommandLine.CommandLineParser.ParseOrThrow("/").Key;
+            Assert.True(string.IsNullOrEmpty(aString), "No input was set");
+        }
+        
+        [TestCase("test1")]
+        public void TestCanParseAnyInputAndWriteIntoKey(string key)
+        {
+            var aString = CommandLine.CommandLineParser.ParseOrThrow($"/{key}").Key;
+            Assert.AreEqual(key, aString);
+        }
+        
+        [TestCase("test1","test2")]
+        [TestCase("tessdft1","tesdft2")]
+        [TestCase("tesaffaat1","testafdaf2")]
+        [TestCase("abc","def")]
+        public void TestCanParseKeyValueEntry(string key, string value)
+        {
+            var result = CommandLine.CommandLineParser.ParseOrThrow($"/{key}={value}");
+            var keyResult = result.Key;
+            var valueResult = result.Value;
+            Assert.AreEqual(key, keyResult);
+            Assert.AreEqual(value, valueResult);
+        }
+        
+        [TestCase("","test2")]
+        [TestCase("tessdft1","")]
+        public void TestCannotParseEmptyKeyOrValue(string key, string value)
+        {
+            Assert.Throws<ParseException>(() => CommandLine.CommandLineParser.ParseOrThrow($"/{key}={value}"));
         }
     }
 }
